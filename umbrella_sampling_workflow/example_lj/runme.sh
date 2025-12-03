9386ab0dc1fb45f3f8eb1d166f89c32ad3f8a21e
Amrita Goswami <amrita16thaug646@gmail.com>
2025-12-03 11:05:28 +0100

    workflow: WHAM meta file written out
    Also fixed bug in the LAMMPS template_lj (clear was removing the
    box size information too)

--- umbrella_sampling_workflow/example_lj/runme.sh
+++ umbrella_sampling_workflow/example_lj/runme.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-micromamba activate fittingenv
 snakemake --cores 1 --printshellcmds --keep-incomplete --keep-going --config mpi_threads=1 --snakefile ../workflow/snakefile --directory .
